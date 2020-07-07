bd630d9fb2182ccfedfbec06dabd60a22dfbe53f
Fred Cox <fredcox@gmail.com>
2020-07-07 18:36:09 -0300

    finish the bash scripts

---
+++ scripts/rnd4kq1t1read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+TARGET="$1"
+me=`basename "$0"`
+
+fio --loops=1 --size=512k --filename="$TARGET/fiomark.tmp" --stonewall --ioengine=libaio --direct=1 \
+--name="$me" --bs=4k --iodepth=1 --numjobs=1 --rw=randread
+
+rm "$TARGET/fiomark.tmp"
