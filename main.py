e862669ffa1de502e05c39b520eb7a65ab63541d
Fred Cox <fredcox@gmail.com>
2020-07-07 21:32:21 -0300

    maybe finish in this week

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from gi.repository import Gtk
 
 
 class Handler:
+    benchmarkRunning = False
+
     def __init__(self):
         print("Init...")
         self.directoryChoose = builder.get_object("directoryChoose")
@@ -38,8 +40,17 @@ class Handler:
     def executeCmd(self, cmd):
         return subprocess.getoutput(cmd)
 
+    def stopBechmark(self):
+        self.benchmarkRunning = False
+        self.runButton.set_label("Run")
+        self.statusDynamicLabel.set_text("IDLE")
+
     def on_runButton_clicked(self, widget):
-        print("Clicou em Run!")
+        self.progressBar.set_fraction(0)
+        if self.benchmarkRunning:
+            self.stopBechmark()
+        else:
+            self.runBenchmark()
 
     def on_directoryChoose_file_set(self, widget):
         print("Escolheu um diretório....")
@@ -54,30 +65,42 @@ class Handler:
     def on_aboutMenuItem_activate(self, widget):
         print("Clicou em about...")
 
+    def is_odd(self, a):
+        return bool(a & 1)
+
     def runBenchmark(self):
         print("Running the Benchmark...")
+        self.benchmarkRunning = True
+        self.runButton.set_label("Stop")
+        self.statusDynamicLabel.set_text("Running, please wait...")
         benchmarks = [
-            'seq1mq8t1read',
-            'seq1mq8t1write',
-            'seq1mq1t1read',
-            'seq1mq1t1write',
-            'rnd4kq32t16read',
-            'rnd4kq32t16write',
-            'rnd4kq1t1read',
-            'rnd4kq1t1write'
+            "seq1mq8t1read",
+            "seq1mq8t1write",
+            "seq1mq1t1read",
+            "seq1mq1t1write",
+            "rnd4kq32t16read",
+            "rnd4kq32t16write",
+            "rnd4kq1t1read",
+            "rnd4kq1t1write",
         ]
+        for index, benchmark in enumerate(benchmarks):
+            print(f"Index: [{index}] {benchmark}.sh {self.directoryChoose.get_filename()}")
+            #output = json.loads(
+            #    self.executeCmd(
+            #        f"./scripts/{i}.sh {self.directoryChoose.get_filename()}"
+            #    )
+            #)
 
-        for i in benchmarks:
-            print(f'{benchmarks}.sh')
 
+        self.stopBechmark()
 
-      #  output = json.loads(
-       #     self.executeCmd(
-        #        f"./scripts/rnd4kq1t1read.sh {self.directoryChoose.get_filename()}"
-        #    )
-        #)
-        #bw_bytes = output["jobs"][0]["read"]["bw_bytes"]
-        #print(f"Bytes =====> {humanfriendly.format_size(bw_bytes)}/s")
+    #  output = json.loads(
+    #     self.executeCmd(
+    #        f"./scripts/rnd4kq1t1read.sh {self.directoryChoose.get_filename()}"
+    #    )
+    # )
+    # bw_bytes = output["jobs"][0]["read"]["bw_bytes"]
+    # print(f"Bytes =====> {humanfriendly.format_size(bw_bytes)}/s")
 
 
 builder = Gtk.Builder()
