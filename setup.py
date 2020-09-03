3f9bfca0d270b9c81bda7ef76df6990f01f0fda6
Fred Lins <fredcox@gmail.com>
2020-09-03 14:02:19 -0300

    Release 0.4.7 out now. 🚀🚀🚀🚀🚀🚀

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 # update aboutdialog.ui with correct version
-version = '0.4.7'
+version = '0.4.8'
 pattern = "([0-9]+.[0-9]+.[0-9]+)"
 newlines = []
 dialog_filename = 'crazydiskmark/aboutdialog.ui'
