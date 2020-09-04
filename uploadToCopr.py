c17885d7ec92a7b806542a1c42b1cbc417e86e8a
Fred Lins <fredcox@gmail.com>
2020-09-04 20:01:03 -0300

    added script to upload to copr

---
+++ uploadToCopr.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import coloredlogs
+import logging
+import sys
+
+# Create a logger object.
+# shellcheck disable=SC2034
+logger = logging.getLogger(__name__)
+coloredlogs.install()
+
+if len(sys.argv) == 1:
+    logger.info('Please, specify the version from command line!')
+    sys.exit(0)
+
+version = sys.argv[1]
+cmd = f'copr-cli build crazydiskmark rpmbuild/crazydiskmark-{version}.fc32.src.rpm'
