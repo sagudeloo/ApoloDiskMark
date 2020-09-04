27563918a2b0b5641ff1dfccf7f4887d7da24c8a
Fred Lins <fredcox@gmail.com>
2020-09-04 10:18:32 -0300

    i will use pandoc now

--- publish.sh
+++ publish.sh
@@ -4,6 +4,8 @@ rm -rfv dist/
 rm -rfv build/
 rm -rfv crazydiskmark.egg-info/
 
+pandoc -r markdown -w plain -o README.me README.md
+
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
 
