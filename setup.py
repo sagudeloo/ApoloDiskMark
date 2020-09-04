2375e7fb1080dbf9a37c72b9fa7d42500c582df9
Fred Lins <fredcox@gmail.com>
2020-09-04 13:51:31 -0300

    prepare to release 0.5.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 # update aboutdialog.ui with correct version
-version = '0.5.5'
+version = '0.5.6'
 pattern = "([0-9]+.[0-9]+.[0-9]+)"
 newlines = []
 dialog_filename = 'crazydiskmark/aboutdialog.ui'
